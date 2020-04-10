1dd38a184e6818a1caf430fd56eb3a3bd0b17dab
Sandro Batista <ctsandrobatista@outlook.com>
2020-04-09 22:17:50 -0300

    git add.

--- app/src/main/java/com/rafaelmfer/nasaexperience/MainActivity.java
+++ app/src/main/java/com/rafaelmfer/nasaexperience/MainActivity.java
@@ -2,13 +2,36 @@ package com.rafaelmfer.nasaexperience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+   TextInputLayout usuario;
+   TextInputLayout senha;
+   Button btnEntrar;
+   Button btnFacebook;
+   Button btnGoogle;
+   Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
