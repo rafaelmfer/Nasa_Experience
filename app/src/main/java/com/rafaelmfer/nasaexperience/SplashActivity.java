22af0aa8792cd41fe6d7c1b33f32fc1100af3f33
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-04-18 23:46:54 -0300

    Ajuste da Splash Activity. Agora construída do jeito certo

--- app/src/main/java/com/rafaelmfer/nasaexperience/SplashActivity.java
+++ app/src/main/java/com/rafaelmfer/nasaexperience/SplashActivity.java
@@ -2,7 +2,6 @@ package com.rafaelmfer.nasaexperience;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,19 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-//
-//        getSupportActionBar().hide();
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getBaseContext(), LoginActivity.class));
-                finish();
-
-            }
-        }, 1000);
-
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
