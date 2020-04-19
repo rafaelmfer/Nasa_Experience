d6469d386d697dc9cb7e57603f26476d7d9552de
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-04-19 02:10:20 -0300

    Ajustes na Activity Login, inclusão da validação dos campos de Email e Password

--- app/src/main/java/com/rafaelmfer/nasaexperience/LoginActivity.java
+++ app/src/main/java/com/rafaelmfer/nasaexperience/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,15 +12,34 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextInputLayout tilLoginUserName, tilLoginUserPassword;
-    Button btSignIn, btFacebookSignIn, btGoogleSignIn, btSignUp;
+    TextInputLayout tilLoginUserEmail, tilLoginUserPassword;
+    EditText etLoginUserEmail, etLoginUserPassword;
+    Button btLogin, btFacebookSignIn, btGoogleSignIn, btSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        bindViews();
 
-        btSignUp = findViewById(R.id.btSignUp);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tilLoginUserEmail.setError(null);
+                tilLoginUserPassword.setError(null);
+                if (editTextIsNotEmpty(etLoginUserEmail, etLoginUserPassword)) {
+                    //TODO start HomeActivity
+//                    startActivity(new Intent(view.getContext(), HomeActivity.class));
+                } else {
+                    if (!editTextIsNotEmpty(etLoginUserEmail)) {
+                        tilLoginUserEmail.setError(getString(R.string.error_field_must_be_filled));
+                    }
+                    if (!editTextIsNotEmpty(etLoginUserPassword)) {
+                        tilLoginUserPassword.setError(getString(R.string.error_field_must_be_filled));
+                    }
+                }
+            }
+        });
 
         btSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +48,25 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    private void bindViews() {
+        tilLoginUserEmail = findViewById(R.id.tilLoginUserEmail);
+        tilLoginUserPassword = findViewById(R.id.tilLoginUserPassword);
+        etLoginUserEmail = findViewById(R.id.etLoginUserEmail);
+        etLoginUserPassword = findViewById(R.id.etLoginUserPassword);
+        btLogin = findViewById(R.id.btLogin);
+        btFacebookSignIn = findViewById(R.id.btFacebookSignIn);
+        btGoogleSignIn = findViewById(R.id.btGoogleSignIn);
+        btSignUp = findViewById(R.id.btSignUp);
+    }
+
+    private boolean editTextIsNotEmpty(EditText... editTexts) {
+        for (EditText et : editTexts) {
+            if (et.getText().toString().isEmpty()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
