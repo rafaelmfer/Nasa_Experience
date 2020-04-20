abddfb4c510e660b5ca4bffcd3d34c66e09a9814
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-04-20 01:02:46 -0300

    Criação da Tela de Monitorar Asteróides, a colocada a função do botão de voltar.

---
+++ app/src/main/java/com/rafaelmfer/nasaexperience/AsteroidsNearFromEarthActivity.java
@@ -0,0 +1,27 @@
+package com.rafaelmfer.nasaexperience;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AsteroidsNearFromEarthActivity extends AppCompatActivity {
+
+    ImageButton ibAsteroidsBack;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asteroids_near_from_earth);
+
+        ibAsteroidsBack = findViewById(R.id.ibAsteroidsBack);
+        ibAsteroidsBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
