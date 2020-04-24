7a1e9d20548b286840989f8a85e035d4a59c47d5
Sandro Batista <ctsandrobatista@outlook.com>
2020-04-24 18:26:34 -0300

    Sprint2

---
+++ app/src/main/java/com/rafaelmfer/nasaexperience/HomeScreen.java
@@ -0,0 +1,52 @@
+package com.rafaelmfer.nasaexperience;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class HomeScreen extends AppCompatActivity {
+     Button btn_MarsWeather, btn_Asteroids, btn_Astronomic;
+     ImageButton imgBtn_Menu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        imgBtn_Menu = findViewById(R.id.imgBtn_Menu);
+        btn_MarsWeather = findViewById(R.id.btn_MarsWeather);
+        btn_Asteroids = findViewById(R.id.btn_Asteroids);
+        btn_Astronomic = findViewById(R.id.btn_Astronomic);
+
+        btn_MarsWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn_Asteroids.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn_Astronomic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        imgBtn_Menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/rafaelmfer/nasaexperience/RegisterActivity.java
+++ app/src/main/java/com/rafaelmfer/nasaexperience/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.rafaelmfer.nasaexperience;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RegisterActivity extends AppCompatActivity {
 
     ImageButton btRegisterBack;
+    Button btCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +19,21 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         btRegisterBack = findViewById(R.id.btRegisterBack);
+        btCreateAccount = findViewById(R.id.btCreateAccount);
 
         btRegisterBack.setOnClickListener(new View.OnClickListener(){
            @Override
             public void onClick(View view){
                onBackPressed();
            }
+        });
+
+        btCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, HomeScreen.class);
+                startActivity(intent);
+            }
         });
     }
 }
