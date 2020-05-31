6cc7164dfc572ac2ae9224438f9a70fd5a6c32eb
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-05-31 20:45:28 -0300

    Refactor da classe pra usar ViewBinding usando a ActBind

--- app/src/main/java/com/rafaelmfer/nasaexperience/ui/activity/ActivityMarsWeather.java
+++ app/src/main/java/com/rafaelmfer/nasaexperience/ui/activity/ActivityMarsWeather.java
@@ -1,41 +1,40 @@
 package com.rafaelmfer.nasaexperience.ui.activity;
 
-import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.rafaelmfer.nasaexperience.R;
+import com.rafaelmfer.nasaexperience.baseviews.ActBind;
+import com.rafaelmfer.nasaexperience.databinding.ActivityMarsWeatherBinding;
 import com.rafaelmfer.nasaexperience.model.PostsMars;
 import com.rafaelmfer.nasaexperience.ui.adapter.AdapterMars;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ActivityMarsWeather extends AppCompatActivity {
+public class ActivityMarsWeather extends ActBind<ActivityMarsWeatherBinding> {
 
-    RecyclerView recyclerView_mars;
-    ImageButton ibMarsWeatherBack;
     List<PostsMars> posts = new ArrayList<>();
 
+    @NotNull
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mars_weather);
-        recyclerView_mars = findViewById(R.id.recycler_mars);
+    public Class<ActivityMarsWeatherBinding> getBindClass() {
+        return ActivityMarsWeatherBinding.class;
+    }
+
+    @Override
+    public void onBoundView(@NotNull ActivityMarsWeatherBinding binding) {
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView_mars.setLayoutManager(layoutManager);
+        binding.recyclerMars.setLayoutManager(layoutManager);
         newPosts();
         AdapterMars adapterMars = new AdapterMars(posts);
-        recyclerView_mars.setAdapter(adapterMars);
+        binding.recyclerMars.setAdapter(adapterMars);
 
-        ibMarsWeatherBack = findViewById(R.id.mars_weather_back);
-        ibMarsWeatherBack.setOnClickListener(new View.OnClickListener() {
+        binding.marsWeatherBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
