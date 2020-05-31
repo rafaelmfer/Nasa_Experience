02352f7edafff4efeef715495c74f2b06c29fb90
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-05-31 19:55:39 -0300

    Repository e ServiceNasa com o endpoint do Serviço Climático de Marte para o retrofit criado

---
+++ app/src/main/java/com/rafaelmfer/nasaexperience/repository/RepositoryNasa.kt
@@ -0,0 +1,14 @@
+package com.rafaelmfer.nasaexperience.repository
+
+import com.rafaelmfer.nasaexperience.model.marsweatherservice.MarsWeatherResponse
+
+class RepositoryNasa {
+
+    private var url = "https://api.nasa.gov/"
+    private var service = ServiceNasaAPIs::class
+    private val apiKey = "VHr3hJIcoZkU0JJo1hGUL7NW1nvpX9JMwHwLOL0n"
+
+    private val serviceRick = RetroInit(url).create(service)
+
+    suspend fun getCharacters(): MarsWeatherResponse = serviceRick.getMarsWeather(apiKey = apiKey)
+}
