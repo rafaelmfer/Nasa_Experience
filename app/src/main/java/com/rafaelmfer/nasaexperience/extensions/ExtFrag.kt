7a8cd0c334f8aeabf937385b55519bf6d75d1604
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-05-31 20:21:58 -0300

    Extensions de Fragment

---
+++ app/src/main/java/com/rafaelmfer/nasaexperience/extensions/ExtFrag.kt
@@ -0,0 +1,30 @@
+package com.rafaelmfer.nasaexperience.extensions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+
+fun <T : Fragment> T.newInstance(bundle: Bundle? = null): T {
+    arguments = bundle
+    return this
+}
+
+fun <T : Fragment> T.newInstance(bundleBuilder: Bundle.() -> Unit) = apply {
+    Bundle().let {
+        bundleBuilder.invoke(it)
+        arguments = it
+    }
+}
+
+fun Fragment.requireAct(block: Fragment.() -> Unit) =
+    requireActivity().run { block.invoke(this@requireAct) }
+
+@Suppress("DEPRECATION")
+inline fun <reified Model : ViewModel> FragmentActivity.viewModel(): Model =
+    ViewModelProviders.of(this).get(Model::class.java)
+
+@Suppress("DEPRECATION")
+inline fun <reified Model : ViewModel> Fragment.viewModel(): Model =
+    ViewModelProviders.of(this).get(Model::class.java)
