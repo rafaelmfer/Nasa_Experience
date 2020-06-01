8968856af37196f3d01e8dfc048216bd3c4ebaf7
Rafinhaa32 <rafaelmfer@hotmail.com>
2020-06-01 02:44:14 -0300

    Transformação da ActivityMarsWeather para Kotlin, Criação da ViewModelMarsWeather, chamada do endpoint na viewModel q depois passa os dados para o adapter

--- app/src/main/java/com/rafaelmfer/nasaexperience/ui/adapter/AdapterMars.java
+++ app/src/main/java/com/rafaelmfer/nasaexperience/ui/adapter/AdapterMars.java
@@ -9,15 +9,18 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.rafaelmfer.nasaexperience.R;
-import com.rafaelmfer.nasaexperience.model.PostsMars;
-
-import java.util.List;
+import com.rafaelmfer.nasaexperience.model.marsweatherservice.MarsWeatherResponse;
 
 public class AdapterMars extends RecyclerView.Adapter<AdapterMars.MyViewHolderMars> {
-    public List<PostsMars> posts;
 
-    public AdapterMars(List<PostsMars> listPosts) {
-        this.posts = listPosts;
+    public MarsWeatherResponse marsWeather = new MarsWeatherResponse();
+
+    public AdapterMars() {
+    }
+
+    public void setMarsWeather(MarsWeatherResponse marsWeather) {
+        this.marsWeather = marsWeather;
+        notifyDataSetChanged();
     }
 
     @NonNull
@@ -30,17 +33,58 @@ public class AdapterMars extends RecyclerView.Adapter<AdapterMars.MyViewHolderMa
 
     @Override
     public int getItemCount() {
-        return posts.size();
+        return marsWeather.getSolKeys().size();
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolderMars holder, int position) {
-        PostsMars post = posts.get(position);
-        holder.tvSunsNumberMars.setText(post.getSunsNumberMars());
-        holder.tvDateObservationMars.setText(post.getDateObservationMars());
-        holder.tvPressure.setText(post.getPressure());
-        holder.tvWindSpeed.setText(post.getWindSpeed());
-        holder.tvTemperature.setText(post.getTemperature());
+        String suns = marsWeather.getSolKeys().get(position);
+        holder.tvSunsNumberMars.setText(suns);
+
+        switch (position) {
+            case 0:
+                holder.tvDateObservationMars.setText(marsWeather.getSunNumber1().getFirstUTC());
+                holder.tvPressure.setText(String.valueOf(marsWeather.getSunNumber1().getAtmosphericPressure().getAv()));
+                holder.tvWindSpeed.setText(String.valueOf(marsWeather.getSunNumber1().getHorizontalWindSpeed().getAv()));
+                holder.tvTemperature.setText(String.valueOf(marsWeather.getSunNumber1().getAtmosphericTemperature().getAv()));
+                break;
+            case 1:
+                holder.tvDateObservationMars.setText(marsWeather.getSunNumber2().getFirstUTC());
+                holder.tvPressure.setText(String.valueOf(marsWeather.getSunNumber2().getAtmosphericPressure().getAv()));
+                holder.tvWindSpeed.setText(String.valueOf(marsWeather.getSunNumber2().getHorizontalWindSpeed().getAv()));
+                holder.tvTemperature.setText(String.valueOf(marsWeather.getSunNumber2().getAtmosphericTemperature().getAv()));
+                break;
+            case 2:
+                holder.tvDateObservationMars.setText(marsWeather.getSunNumber3().getFirstUTC());
+                holder.tvPressure.setText(String.valueOf(marsWeather.getSunNumber3().getAtmosphericPressure().getAv()));
+                holder.tvWindSpeed.setText(String.valueOf(marsWeather.getSunNumber3().getHorizontalWindSpeed().getAv()));
+                holder.tvTemperature.setText(String.valueOf(marsWeather.getSunNumber3().getAtmosphericTemperature().getAv()));
+                break;
+            case 3:
+                holder.tvDateObservationMars.setText(marsWeather.getSunNumber4().getFirstUTC());
+                holder.tvPressure.setText(String.valueOf(marsWeather.getSunNumber4().getAtmosphericPressure().getAv()));
+                holder.tvWindSpeed.setText(String.valueOf(marsWeather.getSunNumber4().getHorizontalWindSpeed().getAv()));
+                holder.tvTemperature.setText(String.valueOf(marsWeather.getSunNumber4().getAtmosphericTemperature().getAv()));
+                break;
+            case 4:
+                holder.tvDateObservationMars.setText(marsWeather.getSunNumber5().getFirstUTC());
+                holder.tvPressure.setText(String.valueOf(marsWeather.getSunNumber5().getAtmosphericPressure().getAv()));
+                holder.tvWindSpeed.setText(String.valueOf(marsWeather.getSunNumber5().getHorizontalWindSpeed().getAv()));
+                holder.tvTemperature.setText(String.valueOf(marsWeather.getSunNumber5().getAtmosphericTemperature().getAv()));
+                break;
+            case 5:
+                holder.tvDateObservationMars.setText(marsWeather.getSunNumber6().getFirstUTC());
+                holder.tvPressure.setText(String.valueOf(marsWeather.getSunNumber6().getAtmosphericPressure().getAv()));
+                holder.tvWindSpeed.setText(String.valueOf(marsWeather.getSunNumber6().getHorizontalWindSpeed().getAv()));
+                holder.tvTemperature.setText(String.valueOf(marsWeather.getSunNumber6().getAtmosphericTemperature().getAv()));
+                break;
+            case 6:
+                holder.tvDateObservationMars.setText(marsWeather.getSunNumber7().getFirstUTC());
+                holder.tvPressure.setText(String.valueOf(marsWeather.getSunNumber7().getAtmosphericPressure().getAv()));
+                holder.tvWindSpeed.setText(String.valueOf(marsWeather.getSunNumber7().getHorizontalWindSpeed().getAv()));
+                holder.tvTemperature.setText(String.valueOf(marsWeather.getSunNumber7().getAtmosphericTemperature().getAv()));
+                break;
+        }
     }
 
     public static class MyViewHolderMars extends RecyclerView.ViewHolder {
